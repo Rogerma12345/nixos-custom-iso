9a12b633a21f0a9f59f47f78b1f461300861c0bb
Rogerma12345 <121156762+Rogerma12345@users.noreply.github.com>
2026-04-02 07:07:39 +0800

    Refactor mk-user.nix syntax and structure

--- nix/lib/mk-user.nix
+++ nix/lib/mk-user.nix
@@ -1,11 +1,8 @@
-{ lib }:
-{ username, authorizedKeys ? [] }:
-{
+{ lib }: { username, authorizedKeys ? [] }: {
   users.users.${username} = {
     isNormalUser = true;
     description = "Default installer user";
     extraGroups = [ "wheel" ];
-    shell = lib.mkDefault null;
     hashedPassword = "!";
     openssh.authorizedKeys.keys = authorizedKeys;
   };
