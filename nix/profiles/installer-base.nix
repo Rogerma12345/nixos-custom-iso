46d24de00dd6e86817265ba1c9da97ba33eafdb6
Rogerma12345 <121156762+Rogerma12345@users.noreply.github.com>
2026-04-02 14:52:16 +0800

    Remove unused module imports from installer-base.nix
    Removed unused module imports for docker, python, and wolfram.

--- nix/profiles/installer-base.nix
+++ nix/profiles/installer-base.nix
@@ -1,17 +1,14 @@
 { modulesPath, ... }:
+
 {
   imports = [
     (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
     ../modules/base.nix
-    ../modules/docker.nix
-    ../modules/python.nix
-    ../modules/wolfram.nix
   ];
 
   isoImage.isoName = "nixos-custom-iso-installer.iso";
   isoImage.makeEfiBootable = true;
   isoImage.makeUsbBootable = true;
   isoImage.squashfsCompression = "zstd -Xcompression-level 8";
-
   documentation.enable = false;
 }
