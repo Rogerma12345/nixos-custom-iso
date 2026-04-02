bfb48463f648dee78fe42e97d485c0ef67b4cec1
Rogerma12345 <121156762+Rogerma12345@users.noreply.github.com>
2026-04-02 08:07:11 +0800

    Update python.nix

--- nix/modules/python.nix
+++ nix/modules/python.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, ... }:
 let
   installerSettings = import ../lib/installer-settings.nix;
-  settings = installerSettings {};
+  settings = installerSettings { custom = true; };
 in
 lib.mkIf settings.features.python {
   environment.systemPackages = with pkgs; [
